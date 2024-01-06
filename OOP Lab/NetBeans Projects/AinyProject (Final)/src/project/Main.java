b181dcd9198cb30d92f61fafddbbe930d85c8c1b
Ibtisam Afzal <chaudhryibtisam2003@gmail.com>
2024-01-06 11:57:28 +0500

    Theory Added

--- OOP Lab/NetBeans Projects/AinyProject (Final)/src/project/Main.java
+++ OOP Lab/NetBeans Projects/AinyProject (Final)/src/project/Main.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package project;
 
 /**
  *
  * @author Qurratul Ain
  */
+
 public class Main extends javax.swing.JFrame {
 
     /**
